266961f237413a85fc29e02be8005adb78afc497
Daniel Yang <danyang0724@gmail.com>
2022-05-18 22:06:15 -0400

    commit tree c89a13d3dc1e75b01ef0c359a8faa35f361b5e5a

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // code your solution here
+function superbowlWin(record) {
+    const found = record.find(element => element.result === "W")
+    if(found === undefined) {
+        return undefined;
+    } else {
+        return found.year;
+    }
+}
